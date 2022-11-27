8ff1012093974fd4d687891b125bb20487a249d8
Alan <alanbiju154@gmail.com>
2022-11-27 17:44:35 +0530

    code updated

---
+++ myapi.py
@@ -0,0 +1,15 @@
+
+from fastapi import FastAPI
+app= FastAPI()
+
+@app.get("/")
+def index():
+    return {"name": "Alan"}
+
+@app.get("/about")
+def about():
+    return {"data": "about page"}
+
+@app.get("/get-students/{student_id}")
+def get_students(student_id: int):
+    return {"student_id_num": student_id}  
