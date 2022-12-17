97ceb63e6a91212050f3066977f8a667f903382f
Alan Biju <92132176+a-1an@users.noreply.github.com>
2022-12-18 04:54:53 +0530

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -1,34 +1,39 @@
+
+import os
 from fastapi import FastAPI
 from model import postschema
-app= FastAPI()
+from pymongo import MongoClient
+CONNECTION_STRING = os.getenv(secretone) 
+myclient = MongoClient(CONNECTION_STRING)
+mydb = myclient["Erudite"]
 
-all_posts=[{"id":1,"title": "My first post","desc": "This is my first post"},{ "id":2,"title": "My second post","desc": "This is my second post"}]
-# def frozendict(d: dict):
-#     return frozenset(d.keys()), frozenset(d.values())
+app= FastAPI()
 @app.get("/")
 def index():
     return {"name": "Alan"}
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
+@app.get("/build/{cname}")
+def build_company(cname):
+    mydb.create_collection(cname)
+    return {"company": cname}
 
-@app.get("/get-students/{student_id}")
-def get_students(student_id: int):
-    return {"student_id_num": student_id}  
+@app.get("/addto/{cname}/{sid}/{loc}")
+def add_data(cname,sid,loc):
+   document = {
+    "serialid": sid,
+    "location": loc,
+   }
+   mydb[cname].insert_one(document)
 
-@app.get("/posts/all")
-def all():
-    return {"data": all_posts}
+@app.get("/query/{cname}/{sid}/{loc}")
+def query_data(cname,sid,loc):
+   query= {"serialid": sid,"location": loc }
+   document = mydb[cname].find_one(query)
+   if document:
+      return {"value":1}
+   else:
+      return {"value":0}
+ 
 
-@app.get("/posts/{post_id}")
-def blog(post_id : int):
-    for i in all_posts:
-        if i["id"]==post_id:
-            return {"data":i}
-
-@app.post("/posts/create")
-def create(post: postschema):
-    return {"data": post} 
 
 
